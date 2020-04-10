19102916239aa0aa9f08640ae609c45002e7ae34
123freh <61021014+123freh@users.noreply.github.com>
2020-04-10 18:20:55 +0200

    Update Enemy.py

--- Classes/Enemy.py
+++ Classes/Enemy.py
@@ -3,14 +3,14 @@ from random import *
 
 
 class Enemy(Character):
-    def __init__(self, heart_number, pos_x, pos_y, speed, hp):
-        super().__init__(heart_number, pos_x, pos_y, speed, hp)
+    def __init__(self, pos_x, pos_y, speed, hp):
+        super().__init__(pos_x, pos_y, speed, hp)
         self.drop_list = []  # items, todavía por definir, lista de objetos (clase)
 
-    def drop(self, drop_list):
+    def drop(self):
         """
         Elige un item al azar del drop list y lo retorna
         List -> String
         """
-        return drop_list[randint(0, len(drop_list)-1)]  # no tengo en cuenta droprate
+        return self.drop_list[randint(0, len(self.drop_list)-1)]  # no tengo en cuenta droprate
 
