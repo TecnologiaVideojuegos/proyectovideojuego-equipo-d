f69852261fdec60a8bb81ce71630cb640d676a65
123freh <61021014+123freh@users.noreply.github.com>
2020-04-10 17:29:43 +0200

    Update MainCharacter.py

--- Classes/MainCharacter.py
+++ Classes/MainCharacter.py
@@ -2,9 +2,10 @@ from JOTPK.Classes import Character
 from JOTPK.Classes import Weapon
 
 class MainCharacter(Character):
-    def __init__(self, heart_number, pos_x, pos_y, speed, hp, money):
-        super().__init__(heart_number, pos_x, pos_y, speed, hp)
+    def __init__(self, pos_x, pos_y, speed, hp, money):
+        super().__init__(pos_x, pos_y, speed, hp)
         self.money = money
+        self.weapon = Weapon
 
     def get_money(self):
         """
@@ -12,23 +13,11 @@ class MainCharacter(Character):
         """
         return self.money
 
-    def get_heart_number(self):
-        """
-        Retorna el numero de vidas que tiene el personaje
-        """
-        return self.heart_number
-
     def get_speed(self):
         """
         Retorna la velocidad del personaje
         """
         return self.speed
-
-    def set_heart_number(self, new_heart_number):
-        """
-        Cambia el numero de vidas del personaje
-        """
-        self.heart_number = new_heart_number
 
     def set_speed(self, new_speed):
         """
