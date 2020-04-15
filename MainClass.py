9c05d8276dcefe0ff10ac4804e0756d503bcf371
jorgefernandezr <jorge.fernandezr@edu.uah.es>
2020-04-15 11:14:03 +0200

    Segundo Commit con el primer mapa

--- MainClass.py
+++ MainClass.py
@@ -9,15 +9,21 @@ class Juego(arcade.Window):
         super().__init__(800, 800, "Nombre del videojuego")
 
         self.suelo_paredes = None
+        self.cosas = None
+        self.obstaculos = None
 
     def setup(self):
         my_map = arcade.tilemap.read_tmx("mapas/archivos tsx/entrada.tmx")
         self.suelo_paredes = arcade.tilemap.process_layer(my_map, "suelo y paredes", 1)
+        self.cosas = arcade.tilemap.process_layer(my_map, "cosas", 1)
+        self.obstaculos = arcade.tilemap.process_layer(my_map, "obstaculos", 1)
 
     def on_draw(self):
         # Renderiza
         arcade.start_render()
         self.suelo_paredes.draw()
+        self.cosas.draw()
+        self.obstaculos.draw()
 
 def main():
 
