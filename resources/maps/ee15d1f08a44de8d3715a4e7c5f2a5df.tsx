fce889522bd69e01b1c2f48632f1dfa90666addc
diegoplaza77 <diego.plaza@edu.uah.es>
2020-05-06 19:13:42 +0200

    Create ee15d1f08a44de8d3715a4e7c5f2a5df.tsx

---
+++ resources/maps/ee15d1f08a44de8d3715a4e7c5f2a5df.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="ee15d1f08a44de8d3715a4e7c5f2a5df" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="ee15d1f08a44de8d3715a4e7c5f2a5df.jpg" width="350" height="350"/>
+</tileset>
