3689a75b77edcc15cb8696c29c5d1fb1771f7d0d
123freh <61021014+123freh@users.noreply.github.com>
2020-06-10 02:18:03 +0200

    nada lo pongo como antes xd, ignorad lo de las clases, he unificado el codigo otra vez

--- Clases (ignorar)/PowerUp.py
+++
@@ -1,9 +0,0 @@
-import arcade
-
-
-class PowerUp(arcade.Sprite):
-    def __init__(self, sprite, pos_x, pos_y, tiempo_vida):
-        super().__init__(sprite)
-        self.center_x = pos_x
-        self.center_y = pos_y
-        self.tiempo_vida = tiempo_vida
